4e6f05d9846d4a4afae961e5f3a9a0e150c56c27
Tuan Kim <kimtuan.nguyen@unicity.com>
2021-09-15 20:42:22 +0700

    update active class for navlink

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -7,11 +7,11 @@ const Sidebar = () => {
     <aside class="col-12 col-md-12 col-lg-2">
       <div class="sidebar box sticky-column is_stuck">
         <ul class="nav">
-          <li class="nav__item"><NavLink to="/"><i class="fas fa-user-alt"></i>About</NavLink></li>
-          <li class="nav__item"><NavLink to="/resume"><i class="far fa-file-alt"></i>Resume</NavLink></li>
-          <li class="nav__item"><NavLink to="/works"><i class="fas fa-briefcase"></i>Works</NavLink></li>
-          <li class="nav__item"><NavLink to="/blog"><i class="fas fa-book"></i>Blog</NavLink></li>
-          <li class="nav__item"><NavLink to="/contact"><i class="fas fa-phone"></i>Contact</NavLink></li>
+          <li class="nav__item"><NavLink exact to="/"><i class="fas fa-user-alt"></i>About</NavLink></li>
+          <li class="nav__item"><NavLink exact to="/resume"><i class="far fa-file-alt"></i>Resume</NavLink></li>
+          <li class="nav__item"><NavLink exact to="/works"><i class="fas fa-briefcase"></i>Works</NavLink></li>
+          <li class="nav__item"><NavLink exact to="/contact"><i class="fas fa-book"></i>Blog</NavLink></li>
+          <li class="nav__item"><NavLink exact to="/blog"><i class="fas fa-phone"></i>Contact</NavLink></li>
         </ul>
       </div>
     </aside>

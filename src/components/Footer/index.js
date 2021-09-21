e1f76e83b3f5d89862ec75c99323bddbc3bde441
Nguyen Cong Danh <danhitpk@gmail.com>
2021-09-21 21:24:00 +0700

    Fix icon, Hover, Console Log

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,6 +3,6 @@ import './index.css';
 
 export default function Footer() {
     return (
-        <footer class="footer">Copyright © 2021 Resume. All Rights Reserved</footer>
+        <footer className="footer">Copyright © 2021 Resume. All Rights Reserved</footer>
     );
 }
--- src/modules/about/index.js
+++ src/modules/about/index.js
@@ -7,56 +7,56 @@ import iconphoto from '../../assets/img/icon-photo.svg';
 
 const About = () => {
   return (
-    <div class="col-12 col-md-12 col-lg-10" id="about" >
-      <div class="box box-content">
-        <div class="pb-2">
-          <h1 class="title title--h1 first-title title__separate">About Me</h1>
+    <div className="col-12 col-md-12 col-lg-10" id="about" >
+      <div className="box box-content">
+        <div className="pb-2">
+          <h1 className="title title--h1 first-title title__separate">About Me</h1>
           <p>I'm Creative Director and UI/UX Designer from Sydney, Australia, working in web development and print media. I enjoy turning complex problems into simple, beautiful and intuitive designs.</p>
           <p>My job is to build your website so that it is functional and user-friendly but at the same time attractive. Moreover, I add personal touch to your product and make sure that is eye-catching and easy to use. My aim is to bring across your message and identity in the most creative way. I created web design for many famous brand companies.</p>
         </div>
-        <div class="mt-1">
-          <h2 class="title title--h3">What I'm Doing</h2>
-          <div class="row">
+        <div className="mt-1">
+          <h2 className="title title--h3">What I'm Doing</h2>
+          <div className="row">
             {/* <!-- Case Item --> */}
-            <div class="col-case-item col-padding">
-              <div class="case-item">
-                <img class="case-item__icon" src={icondesign} alt="" />
+            <div className="col-case-item col-padding">
+              <div className="case-item">
+                <img className="case-item__icon" src={icondesign} alt="" />
                 <div>
-                  <h3 class="title title--h4">Web Design</h3>
-                  <p class="case-item__caption">The most modern and high-quality design made at a professional level.</p>
+                  <h3 className="title title--h4">Web Design</h3>
+                  <p className="case-item__caption">The most modern and high-quality design made at a professional level.</p>
                 </div>
               </div>
             </div>
 
             {/* <!-- Case Item --> */}
-            <div class="col-case-item col-padding">
-              <div class="case-item">
-                <img class="case-item__icon" src={icondev} alt="" />
+            <div className="col-case-item col-padding">
+              <div className="case-item">
+                <img className="case-item__icon" src={icondev} alt="" />
                 <div>
-                  <h3 class="title title--h4">Web Development</h3>
-                  <p class="case-item__caption">High-quality development of sites at the professional level.</p>
+                  <h3 className="title title--h4">Web Development</h3>
+                  <p className="case-item__caption">High-quality development of sites at the professional level.</p>
                 </div>
               </div>
             </div>
 
             {/* <!-- Case Item --> */}
-            <div class="col-case-item col-padding">
-              <div class="case-item">
-                <img class="case-item__icon" src={iconapp} alt="" />
+            <div className="col-case-item col-padding">
+              <div className="case-item">
+                <img className="case-item__icon" src={iconapp} alt="" />
                 <div>
-                  <h3 class="title title--h4">Mobile Apps</h3>
-                  <p class="case-item__caption">Professional development of applications for iOS and Android.</p>
+                  <h3 className="title title--h4">Mobile Apps</h3>
+                  <p className="case-item__caption">Professional development of applications for iOS and Android.</p>
                 </div>
               </div>
             </div>
 
             {/* <!-- Case Item --> */}
-            <div class="col-case-item col-padding">
-              <div class="case-item">
-                <img class="case-item__icon" src={iconphoto} alt="" />
+            <div className="col-case-item col-padding">
+              <div className="case-item">
+                <img className="case-item__icon" src={iconphoto} alt="" />
                 <div>
-                  <h3 class="title title--h4">Photography</h3>
-                  <p class="case-item__caption">I make high-quality photos of any category at a professional level.</p>
+                  <h3 className="title title--h4">Photography</h3>
+                  <p className="case-item__caption">I make high-quality photos of any category at a professional level.</p>
                 </div>
               </div>
             </div>
